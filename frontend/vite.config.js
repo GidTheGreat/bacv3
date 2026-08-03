4e13a8f30f5794cb5e0b8e54f2bd542f788abf9d
Ubuntu <gid@gid.df4pqyoslvcu1feqawzay1rm5d.jnbx.internal.cloudapp.net>
2026-08-03 14:28:05 +0000

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  server: {
+    host: true,
+    proxy: {
+      "/ws": {
+        target: "ws://localhost:8000",
+        ws: true,
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+})
