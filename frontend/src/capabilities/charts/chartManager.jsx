7e63fd64a15c835607e3c97ce442dfac124cdf39
Ubuntu <gid@gid.tail57c1fc.ts.net>
2026-09-09 14:55:12 +0000

    clamped cursor for replay

--- frontend/src/capabilities/charts/chartManager.jsx
+++ frontend/src/capabilities/charts/chartManager.jsx
@@ -225,8 +225,14 @@ function ChartData({ chartId, fpRef, chartRef, containerRef }) {
         if (!replayStateDeets?.playing) return;
 
         const interval = setInterval(() => {
+           const cursor =
+    replayStateDeets.cursor =
+        Math.min(
+            renderdata.length,
+            replayStateDeets.cursor + 1
+        );
 
-            setReplayState(k1, "cursor", replayStateDeets.cursor+=1)
+            setReplayState(k1, "cursor", cursor)
 
         }, 250 / replayStateDeets.speed);
 
